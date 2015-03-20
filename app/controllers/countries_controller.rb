abb1576b3856fe8737180074b8f79da940e39f83
George Michokostas <michokostas@gmail.com>
2015-03-20 09:54:39 +0100

    move functionality to the controller

--- app/controllers/countries_controller.rb
+++ app/controllers/countries_controller.rb
@@ -1,7 +1,10 @@
 class CountriesController < ApplicationController
 
 	def index
+		@country_names = []
 		@countries = Country.all
+		@countries.each { |country| @country_names << country.country_name }
+		@country_names = @country_names.uniq
 	end
 
 end
