f85061790e0d3b4e239e07400f7524512937f604
George Michokostas <michokostas@gmail.com>
2015-03-22 14:41:50 +0100

    list/edit/destroy routes

--- app/controllers/routes_controller.rb
+++ app/controllers/routes_controller.rb
@@ -1,10 +1,38 @@
 class RoutesController < ApplicationController
 
+	def index
+		@routes = Route.all
+	end
+
+	def show
+		@route = Route.find params[:id]
+		@country = Country.find @route.country_id
+	end
+
+	def edit
+		@route = Route.find params[:id]
+	end
+
+	def update
+		@route = Route.find params[:id]
+		if @route.update route_params
+			redirect_to @route
+		else
+			render :edit
+		end
+	end
+
 	def create
 		@country = Country.find params[:country_id]
 		@route = @country.routes.create route_params
 		flash[:notice] = "Route succesfully added"
 		render 'routes/show', locals: {route: @route, country: @country}
+	end
+
+	def destroy
+		@route = Route.find params[:id]
+		@route.destroy
+		redirect_to routes_path
 	end
 
 	private
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
 	root 'welcome#index'
 
 	resources :countries do
-		resources :routes
+		resources :routes, only: [:show, :create, :new]
 	end
+
+	resources :routes, only: [:index, :edit, :update, :destroy, :show]
 
 end
