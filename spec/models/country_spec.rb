86365c29f41672057ef8364c97f6d918c3389e4a
george <george@devolute.org>
2015-03-25 18:51:44 +0100

    add some expectations on route model test

--- spec/models/country_spec.rb
+++ spec/models/country_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Country, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it "has the right columns" do
+    expect have_db_column(:name).of_type(:string).with_options({})
+  end
+
+  specify("must only have the right columns") do
+    expect(Country.column_names) =~ ["name"]
+  end
+
 end
