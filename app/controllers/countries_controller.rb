438c8f3d1ea830d8b189726c1a26d8fa7e37015a
george <george@devolute.org>
2015-03-27 16:26:58 +0100

    beautify routes & countries controllers

--- app/controllers/countries_controller.rb
+++ app/controllers/countries_controller.rb
@@ -1,59 +1,59 @@
 class CountriesController < ApplicationController
 
-	def show
-		@country = Country.find params[:id]
-	end
+  def show
+    @country = Country.find params[:id]
+  end
 
-	def new
-		@country = Country.new
-	end
+  def new
+    @country = Country.new
+  end
 
-	def edit
-		@country = Country.find params[:id]
-	end
+  def edit
+    @country = Country.find params[:id]
+  end
 
-	def update
-		@country = Country.find params[:id]
-		if @country.update country_params
-			@update = true
-			flash[:notice] = "Country succesfully updated"
-			render 'show', locals: {country: @country, update: @update}
-		else
-			render :edit
-		end
-	end
+  def update
+    @country = Country.find params[:id]
+    if @country.update country_params
+      @update = true
+      flash[:notice] = "Country succesfully updated"
+      render 'show', locals: {country: @country, update: @update}
+    else
+      render :edit
+    end
+  end
 
-	def destroy
-		@country = Country.find params[:id]
-		@country.destroy
-		redirect_to countries_path
-	end
+  def destroy
+    @country = Country.find params[:id]
+    @country.destroy
+    redirect_to countries_path
+  end
 
-	def create
-		country_name = params.fetch(:country)[:name].capitalize
-		@country = Country.find_by(name: country_name)
-		if @country
-			flash[:notice] = "Country exists"
-			render 'show', locals: {country: @country, update: false}
-		else
-			@country = Country.create country_params
-			if @country.save
-				flash[:notice] = "Country succesfully added"
-				render 'show', locals: {country: @country, update: false}
-			else
-				render :new
-			end
-		end
-	end
+  def create
+    country_name = params.fetch(:country)[:name].capitalize
+    @country = Country.find_by(name: country_name)
+    if @country
+      flash[:notice] = "Country exists"
+      render 'show', locals: {country: @country, update: false}
+    else
+      @country = Country.create country_params
+      if @country.save
+        flash[:notice] = "Country succesfully added"
+        render 'show', locals: {country: @country, update: false}
+      else
+        render :new
+      end
+    end
+  end
 
-	def index
-		@countries = Country.all
-	end
+  def index
+    @countries = Country.all
+  end
 
-	private
+  private
 
-	def country_params
-		params.require(:country).permit(:name)
+  def country_params
+    params.require(:country).permit(:name)
   end
 
 end
--- app/controllers/routes_controller.rb
+++ app/controllers/routes_controller.rb
@@ -1,53 +1,53 @@
 class RoutesController < ApplicationController
 
-	def index
-		@routes = Route.all
-	end
+  def index
+    @routes = Route.all
+  end
 
-	def show
-		@route = Route.find params[:id]
-		@country = Country.find @route.country_id
-	end
+  def show
+    @route = Route.find params[:id]
+    @country = Country.find @route.country_id
+  end
 
-	def edit
-		@route = Route.find params[:id]
-	end
+  def edit
+    @route = Route.find params[:id]
+  end
 
-	def new
-		@country = Country.find params[:country_id]
-		@route = Route.new
-	end
+  def new
+    @country = Country.find params[:country_id]
+    @route = Route.new
+  end
 
-	def update
-		@route = Route.find params[:id]
-		if @route.update route_params
-			redirect_to @route
-		else
-			render :new
-		end
-	end
+  def update
+    @route = Route.find params[:id]
+    if @route.update route_params
+      redirect_to @route
+    else
+      render :new
+    end
+  end
 
-	def create
-		@country = Country.find params[:country_id]
-		@route = @country.routes.create route_params
-		if @route.save
-			flash[:notice] = "Route succesfully added"
-			redirect_to route_path(@route)
-		else
-			render 'countries/show', locals: {country: @country, update: false}
-		end
-	end
+  def create
+    @country = Country.find params[:country_id]
+    @route = @country.routes.create route_params
+    if @route.save
+      flash[:notice] = "Route succesfully added"
+      redirect_to route_path(@route)
+    else
+      render 'countries/show', locals: {country: @country, update: false}
+    end
+  end
 
-	def destroy
-		@route = Route.find params[:id]
-		@route.destroy
-		redirect_to routes_path
-	end
+  def destroy
+    @route = Route.find params[:id]
+    @route.destroy
+    redirect_to routes_path
+  end
 
-	private
+  private
 
-	def route_params
-		params.require(:route).permit(:name, :grade, :description, :date, :area, :crag)
-	end
+  def route_params
+    params.require(:route).permit(:name, :grade, :description, :date, :area, :crag)
+  end
 
 end
